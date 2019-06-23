2fe4e596ef25d851c4ca9783f2a141dd3f4d5ba3
Marton Borlay <borlaym@gmail.com>
2019-06-23 21:30:45 +0200

    Updates

---
+++ precache-manifest.b85812b00fd2effffbc392fc8fba9128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406b68e45c12da29ea683d1d93a06ea1",
+    "url": "/zhentarim-agents/index.html"
+  },
+  {
+    "revision": "6bd84b088fb893007515",
+    "url": "/zhentarim-agents/static/css/main.30391f4f.chunk.css"
+  },
+  {
+    "revision": "d4d7a6fa1891abb80d65",
+    "url": "/zhentarim-agents/static/js/2.78fa5475.chunk.js"
+  },
+  {
+    "revision": "6bd84b088fb893007515",
+    "url": "/zhentarim-agents/static/js/main.31330bcd.chunk.js"
+  },
+  {
+    "revision": "0d59ed179370c55d4b67",
+    "url": "/zhentarim-agents/static/js/runtime~main.b64ce0ec.js"
+  }
+]);
