4c24150da9e4614cbd29a199c40b59dbf1a4a89c
Marton Borlay <borlaym@gmail.com>
2019-06-23 16:23:58 +0200

    Guests moving around!

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import './App.css';
 import shuffle from './shuffle';
 import * as uuid from 'uuid';
 
+const NUMBER_OF_GUESTS = 25;
+const NUMBER_OF_AGENTS = 2;
+const MAP_SIZE = 10;
+
 type GuestType = 'commoner' | 'agent';
 type Mask = 'rabbit' | 'mouse' | 'fox';
 const masks: Mask[] = ['rabbit', 'mouse', 'fox'];
@@ -11,7 +15,23 @@ type Guest = {
 	type: GuestType,
 	mask: Mask,
 	converted: Boolean,
-	position: [number, number]
+	position: Position
+}
+
+class Position {
+	row: number;
+	col: number;
+	constructor(row: number, col: number) {
+		this.row = row;
+		this.col = col;
+	}
+	copy(modRow: number, modCol: number): Position {
+		return new Position(this.row + modRow, this.col + modCol);
+	}
+	equals(pos: Position): boolean {
+		return this.row === pos.row && this.col === pos.col;
+	}
+
 }
 
 type Turn = Guest[];
@@ -31,28 +51,37 @@ class App extends React.Component {
 	}
 	createInitialState(): Turn {
 		const guests: Turn = []
-		const availablePositions: [number, number][] = [];
-		for (let row = 0; row < 10; row++) {
-			for (let col = 0; col < 10; col++) {
-				availablePositions.push([row, col]);
+		const availablePositions: Position[] = [];
+		for (let row = 0; row < MAP_SIZE; row++) {
+			for (let col = 0; col < MAP_SIZE; col++) {
+				availablePositions.push(new Position(row, col));
 			}
 		}
-		for (let i = 0; i < 20; i++) {
+		for (let i = 0; i < NUMBER_OF_GUESTS; i++) {
 			guests.push({
 				id: uuid.v4(),
 				type: 'commoner',
 				mask: shuffle<Mask>(masks)[0],
 				converted: false,
-				position: shuffle(availablePositions).pop() || [-1, -1]
+				position: shuffle(availablePositions).pop() || new Position(-1, -1)
+			})
+		}
+		for (let i = 0; i < NUMBER_OF_AGENTS; i++) {
+			guests.push({
+				id: uuid.v4(),
+				type: 'agent',
+				mask: shuffle<Mask>(masks)[0],
+				converted: false,
+				position: shuffle(availablePositions).pop() || new Position(-1, -1)
 			})
 		}
 		return guests;
 	}
 	buildTable(): Array<Array<(Guest | null)>> {
 		const table: Array<Array<(Guest | null)>> = []
-		for (let row = 0; row < 10; row++) {
+		for (let row = 0; row < MAP_SIZE; row++) {
 			const row: Array<(Guest | null)> = []
-			for (let col = 0; col < 10; col++) {
+			for (let col = 0; col < MAP_SIZE; col++) {
 				row.push(null)
 			}
 			table.push(row)
@@ -62,24 +91,57 @@ class App extends React.Component {
 			throw Error()
 		}
 		turn.forEach((guest: Guest) => {
-			table[guest.position[0]][guest.position[1]] = guest;
+			table[guest.position.row][guest.position.col] = guest;
 		})
 		return table;
+	}
+	nextTurn = () => {
+		const currentTurn: Turn = this.state.turns[this.state.turns.length - 1];
+		const occupiedSpaces: Position[] = [];
+		const isOccupied = (pos: Position): boolean => {
+			return !!occupiedSpaces.find(space => space.equals(pos));
+		}
+		const newTurn = currentTurn.map((guest: Guest, index, arr) => {
+			const oldPosition = guest.position;
+			const possiblePositions = [
+				oldPosition.copy(-1, -1), oldPosition.copy(-1, 0), oldPosition.copy(-1, 1),
+				oldPosition.copy(0, -1), /* current pos */ oldPosition.copy(0, 1),
+				oldPosition.copy(1, -1), oldPosition.copy(1, 0), oldPosition.copy(1, 1),
+			].filter(pos => {
+				return !pos.equals(oldPosition) && pos.row > -1 && pos.col > -1 && pos.row < MAP_SIZE && pos.col < MAP_SIZE
+			})
+			let newPosition: Position = guest.position.copy(0, 0);
+			do {
+				newPosition = shuffle(possiblePositions)[0]
+			} while (isOccupied(newPosition));
+			return {
+				...guest,
+				position: newPosition
+			}
+		})
+		this.setState({
+			turns: [...this.state.turns, newTurn]
+		})
 	}
 	render() {
 		const allSpaces = this.buildTable();
 		return (
-			<table>
-				{allSpaces.map((row: Array<(Guest | null)>, index) => (
-					<tr key={index}>
-						{row.map((col: Guest | null, index) => (
-							<td>
-								{col ? col.mask.substr(0, 1) : null}
-							</td>
+			<>
+				<table>
+					<tbody>
+						{allSpaces.map((row: Array<(Guest | null)>, index) => (
+							<tr key={index}>
+								{row.map((col: Guest | null, index) => (
+									<td key={index}>
+										{col ? col.mask.substr(0, 1) : null}
+									</td>
+								))}
+							</tr>
 						))}
-					</tr>
-				))}
-			</table>
+					</tbody>
+				</table>
+				<button onClick={this.nextTurn}>Next turn</button>
+			</>
 		)
 	}
 }
